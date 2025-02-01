7b72bd64058c3afeba5e46024a3f8df9d7cf8fc2
JeanAOC <ortegajean138@gmail.com>
2025-01-31 22:11:22 -0500

    component-home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
 import { VitaminaCComponent } from './vitamina-c/vitamina-c.component';
 import { FusionFlexComponent } from './fusion-flex/fusion-flex.component';
+import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
+    { path: '', component: HomeComponent },
     { path: 'vitamina-c', component: VitaminaCComponent },
     { path: 'fusion-flex', component: FusionFlexComponent },
-    { path: '', redirectTo: '/vitamina-c', pathMatch: 'full' },
-    { path: '**', redirectTo: '/vitamina-c' }
+    //{ path: '', redirectTo: '/', pathMatch: 'full' },
+    { path: '**', redirectTo: '' }
 ];
