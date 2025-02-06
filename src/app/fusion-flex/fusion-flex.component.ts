b27ae17f98007eda0bcedebe26b6325e05ff5fa7
JeanAOC <ortegajean138@gmail.com>
2025-02-05 19:03:00 -0500

    carrucel

--- src/app/fusion-flex/fusion-flex.component.ts
+++ src/app/fusion-flex/fusion-flex.component.ts
@@ -1,12 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { NavbarComponent } from '../navbar/navbar.component';
 
 
 @Component({
   selector: 'app-fusion-flex',
+  standalone: true,
   imports: [NavbarComponent],
   templateUrl: './fusion-flex.component.html',
-  styleUrl: './fusion-flex.component.css'
+  styleUrl: './fusion-flex.component.css',
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class FusionFlexComponent implements OnInit {
   ngOnInit(): void {
--- src/app/vitamina-c/vitamina-c.component.ts
+++ src/app/vitamina-c/vitamina-c.component.ts
@@ -1,12 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { NavbarComponent } from '../navbar/navbar.component';
 
 
 @Component({
   selector: 'app-vitamina-c',
+  standalone: true,
   imports: [NavbarComponent],
   templateUrl: './vitamina-c.component.html',
-  styleUrl: './vitamina-c.component.css'
+  styleUrl: './vitamina-c.component.css',
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class VitaminaCComponent implements OnInit {
   ngOnInit(): void {
