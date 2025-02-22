fee4b0d3c211534225b172f37247a63761083688
JeanAOC <ortegajean138@gmail.com>
2025-02-21 21:09:10 -0500

    cambios

--- src/app/whatsapp/whatsapp.component.ts
+++ src/app/whatsapp/whatsapp.component.ts
@@ -1,11 +1,17 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-whatsapp',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './whatsapp.component.html',
   styleUrl: './whatsapp.component.css'
 })
 export class WhatsappComponent {
+  isTooltipVisible: boolean = true; // Estado para controlar la visibilidad del tooltip
 
+  // Método para ocultar el tooltip
+  closeTooltip() {
+    this.isTooltipVisible = false;
+  }
 }
